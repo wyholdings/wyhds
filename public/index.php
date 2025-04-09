9b5b82e929cdc682ee59c145f358a018fe212af1
wyholdings <help@wyhds.com>
2025-04-09 15:09:45 +0900

    오타 수정 및 index 코드 변경

--- public/index.php
+++ public/index.php
@@ -1,14 +1,17 @@
 <?php
+// 1. Composer autoload
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\Router;
+// 2. .env 먼저 불러오기
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
+// 3. 라우터 및 라우트 등록
+use App\Router;
 $router = new Router();
 require_once __DIR__ . '/../routes/web.php';
 
+// 4. 라우팅 디스패치
 $router->dispatch($_SERVER['REQUEST_URI']);
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
 
 ?>
