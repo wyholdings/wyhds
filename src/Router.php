5b0c3f9fc95fb71cddcc9be3d11dd2216f9b44ff
wyholdings <help@wyhds.com>
2025-04-11 16:16:09 +0900

    404 페이지 추가 및 route 변경

--- src/Router.php
+++ src/Router.php
@@ -23,7 +23,12 @@ class Router {
         }
 
         http_response_code(404);
-        echo "404 Not Found";
+
+        $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../views');
+        $twig = new \Twig\Environment($loader);
+
+        echo $twig->render('errors/404.html.twig');
+        exit;
     }
 }
 ?>
