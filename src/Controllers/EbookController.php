a86074afeffd6c367aa8fb47eae3d023f1be0c91
wyholdings <help@wyhds.com>
2025-07-23 12:07:38 +0900

    ebook index 수정

--- src/Controllers/EbookController.php
+++ src/Controllers/EbookController.php
@@ -109,8 +109,8 @@ class EbookController
                 }
             </style>
 
-            <script src='https://cdnjs.cloudflare.com/ajax/libs/turn.js/4.1.0/turn.min.js'></script>
             <script src='https://code.jquery.com/jquery-3.6.0.min.js'></script>
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/turn.js/4.1.0/turn.min.js'></script>
             </head>
             <body>
 
