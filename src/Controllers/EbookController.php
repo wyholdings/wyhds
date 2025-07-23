50a37058bcc0b3100df04d8712a9ce7847b8df5d
wyholdings <help@wyhds.com>
2025-07-23 17:45:09 +0900

    ebook index 수정

--- src/Controllers/EbookController.php
+++ src/Controllers/EbookController.php
@@ -66,7 +66,7 @@ class EbookController
             <html lang='ko'>
             <head>
             <meta charset='UTF-8'>
-            <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=yes'>
             <title>eBook</title>
             <style>
                 body {
