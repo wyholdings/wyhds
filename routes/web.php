b42d91fbaddd0116e1107f539199139defb1dbd4
wyholdings <help@wyhds.com>
2025-11-19 15:17:45 +0900

    오타 수정

--- routes/web.php
+++ routes/web.php
@@ -48,5 +48,4 @@ $router->post('/admin/money/delete', [App\Controllers\MoneyController::class, 'd
 $router->get('/admin/ebook/list', [App\Controllers\EbookController::class, 'list']);
 $router->post('/admin/ebook/upload', [App\Controllers\EbookController::class, 'upload']);
 $router->get('/admin/ebook/download/{ebookId}', [App\Controllers\EbookController::class, 'download']);
-]);
 ?>
